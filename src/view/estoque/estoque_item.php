ead13a857d0861bb83a091fd054266390e85dff5
evertun <evertonalmeida022@gmail.com>
2025-12-03 17:42:27 -0400

    removi card-body, apenas estava deixando o card descentralizado

--- src/view/estoque/estoque_item.php
+++ src/view/estoque/estoque_item.php
@@ -1,44 +1,42 @@
 <!-- Formulário de Cadastro de Item -->
 <div id="cadastrar_item" class="section">
     <div class="card">
-        <div class="card-body">
-            <h4>Cadastrar Novo Item</h4>
-            
-            <div id="mensagem-item"></div>
-            
-            <form id="formCadastroItem">
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label class="form-label">Nome do Item *</label>
-                        <input type="text" name="nome" class="form-control" placeholder="Ex: Toalha de banho" required>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <label class="form-label">Categoria *</label>
-                        <select name="id_categoria" id="selectCategoria" class="form-select" required>
-                            <option value="">Selecione...</option>
-                        </select>
+        <h4>Cadastrar Novo Item</h4>
+
+        <div id="mensagem-item"></div>
+
+        <form id="formCadastroItem">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Nome do Item *</label>
+                    <input type="text" name="nome" class="form-control" placeholder="Ex: Toalha de banho" required>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label class="form-label">Categoria *</label>
+                    <select name="id_categoria" id="selectCategoria" class="form-select" required>
+                        <option value="">Selecione...</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12 mb-3">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="controla_frigobar" value="1" id="frigobarCheck">
+                        <label class="form-check-label" for="frigobarCheck">
+                            Este item é de frigobar
+                        </label>
                     </div>
                 </div>
-                
-                <div class="row">
-                    <div class="col-md-12 mb-3">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="controla_frigobar" value="1" id="frigobarCheck">
-                            <label class="form-check-label" for="frigobarCheck">
-                                Este item é de frigobar
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                
-                <button type="submit" class="btn btn-primary">
-                    <i class="fas fa-save"></i> Cadastrar Item
-                </button>
-                            <button type="button" class="btn btn-secondary" data-section="lista_item">
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+                <i class="fas fa-save"></i> Cadastrar Item
+            </button>
+            <button type="button" class="btn btn-secondary" data-section="lista_item">
                 <i class="fas fa-list"></i> Ver Todas Itens
             </button>
-            </form>
-        </div>
+        </form>
     </div>
 </div>
 
@@ -88,19 +86,19 @@
             <div class="modal-body">
                 <form id="formEditarItem">
                     <input type="hidden" name="id" id="editItemId">
-                    
+
                     <div class="mb-3">
                         <label class="form-label">Nome do Item *</label>
                         <input type="text" name="nome" id="editNomeItem" class="form-control" required>
                     </div>
-                    
+
                     <div class="mb-3">
                         <label class="form-label">Categoria *</label>
                         <select name="id_categoria" id="editSelectCategoria" class="form-select" required>
                             <option value="">Selecione...</option>
                         </select>
                     </div>
-                    
+
                     <div class="mb-3">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="controla_frigobar" value="1" id="editFrigobarCheck">
@@ -109,7 +107,7 @@
                             </label>
                         </div>
                     </div>
-                    
+
                     <button type="submit" class="btn btn-primary">
                         <i class="fas fa-save"></i> Salvar Alterações
                     </button>
